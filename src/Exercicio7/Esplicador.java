53e140e6df8ae9c6a7fbc3bdf2e70dfdaa80d492
Micaela <freitasmic27@gmail.com>
2019-12-18 10:54:35 +0200

    Resolucao de exercicio 7.1 da classe Esplicador

---
+++ src/Exercicio7/Esplicador.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Exercicio7;
+
+/**
+ *
+ * @author Micah
+ */
+public class Esplicador {
+    /*7.1. Declare os seguintes atributos na classe:
+Nome de usuário, data de nascimento, email, código de visita
+*/
+    private String nomeUsuario;
+    private int dataNascimento;
+    private String email;
+    private int codVisita;
+
+}
