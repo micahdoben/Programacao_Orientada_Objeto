dfcd5e0b2dbdcd2dc4cf3c4e58d60e99cef52e77
Micaela <freitasmic27@gmail.com>
2019-12-18 08:56:57 +0200

    Resulacao exercicio3 na classe contacto

---
+++ src/Exercicio3/Contacto.java
@@ -0,0 +1,43 @@
+
+package Exercicio3;
+
+/**
+ *
+ * @author Micah
+ */
+public class Contacto {
+   private String nome;
+   private int numero;
+   private String email;
+
+    public Contacto(String nome, int numero, String email) {
+        this.nome = nome;
+        this.numero = numero;
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+}
