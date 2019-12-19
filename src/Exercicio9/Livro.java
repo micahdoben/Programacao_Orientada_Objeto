aa35a9eb65d362828136146b2f5a7c4152ea9021
Micaela <freitasmic27@gmail.com>
2019-12-19 07:19:56 +0200

    Resolucao do Exercicio9 POO

---
+++ src/Exercicio9/Livro.java
@@ -0,0 +1,97 @@
+
+package Exercicio9;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author Micah
+ */
+public class Livro {
+    private String titulo;
+    private String autor;
+    private double preco;
+    private int codigo;
+
+    public Livro(String titulo, String autor, double preco, int codigo) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.preco = preco;
+        this.codigo = codigo;
+    }
+
+    public Livro() {
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" + "titulo=" + titulo + ", autor=" + autor + ", preco=" + preco + ", codigo=" + codigo + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Livro other = (Livro) obj;
+        if (Double.doubleToLongBits(this.preco) != Double.doubleToLongBits(other.preco)) {
+            return false;
+        }
+        if (this.codigo != other.codigo) {
+            return false;
+        }
+        if (!Objects.equals(this.titulo, other.titulo)) {
+            return false;
+        }
+        if (!Objects.equals(this.autor, other.autor)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+}
