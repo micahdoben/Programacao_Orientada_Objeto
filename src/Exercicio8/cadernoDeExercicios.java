4c5a9bb70f2069e4394a34775c0d5fb83d8a9d06
Micaela <freitasmic27@gmail.com>
2020-01-14 09:24:48 +0200

    resolucao de cadernode Exercicios

--- src/Exercicio8/cadernoDeExercicios.java
+++ src/Exercicio8/cadernoDeExercicios.java
@@ -1,17 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Exercicio8;
 
 /**
  *
  * @author Micah
  */
-public class cadernoDeExercicios {
-   private String nome;
-   private int telefone;
-   private String email;
-   private int dataAniversario;
+public class cadernoDeExercicios extends Pessoa{
+    
+   
 }
